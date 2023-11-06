3e83de8225436486b309ae308378246e0035ab9f
MissNapalm <148738331+MissNapalm@users.noreply.github.com>
2023-11-06 04:11:17 -0600

    Update scene.js

--- scene.js
+++ scene.js
@@ -10,26 +10,83 @@ const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Create a wireframe pyramid
-const pyramidGeometry = new THREE.ConeGeometry(1, 2, 4);
-const pyramidMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-const pyramid = new THREE.Mesh(pyramidGeometry, pyramidMaterial);
-scene.add(pyramid);
+// Set a black background
+renderer.setClearColor(0x000000, 1);
 
-// Set a clear color with alpha
-renderer.setClearColor(0x000000, 0);
+// Create a wireframe object
+const wireframeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+let wireframe = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 4), wireframeMaterial);
+scene.add(wireframe);
 
 // Set the camera position
 camera.position.set(0, 0, 5);
 camera.lookAt(0, 0, 0);
 
+// Define shape options
+const shapeOptions = {
+    cube: new THREE.BoxGeometry(1, 1, 1),
+    pyramid: new THREE.ConeGeometry(1, 2, 4),
+    sphere: new THREE.SphereGeometry(1, 32, 32),
+};
+
+// Initialize shape and rotation parameters
+let currentShape = 'cube';
+let xRotationSpeed = 0.01;
+let yRotationSpeed = 0.01;
+let objectSize = 1;
+
+// Create user input forms
+const createInput = (label, id, initialValue) => {
+    const inputForm = document.createElement("div");
+    inputForm.innerHTML = `${label}: <input type='number' id='${id}' value='${initialValue}'>`;
+    return inputForm;
+};
+
+const xSpeedInput = createInput("Rotation Speed (X-Axis)", "xSpeed", xRotationSpeed);
+const ySpeedInput = createInput("Rotation Speed (Y-Axis)", "ySpeed", yRotationSpeed);
+const objectSizeInput = createInput("Size of Object", "objectSize", objectSize);
+
+// Create shape selector dropdown
+const shapeSelector = document.createElement("div");
+shapeSelector.innerHTML = "Select Shape: <select id='shapeSelect'><option value='cube'>Cube</option><option value='pyramid'>Pyramid</option><option value='sphere'>Sphere</option></select>";
+
+// Append input forms to the document
+document.body.appendChild(xSpeedInput);
+document.body.appendChild(ySpeedInput);
+document.body.appendChild(objectSizeInput);
+document.body.appendChild(shapeSelector);
+
+// Function to update the wireframe object based on user input
+const updateWireframe = () => {
+    scene.remove(wireframe);
+    wireframe = new THREE.Mesh(shapeOptions[currentShape], wireframeMaterial);
+    wireframe.scale.set(objectSize, objectSize, objectSize);
+    scene.add(wireframe);
+};
+
+// Function to handle user input changes
+const handleInputChange = () => {
+    xRotationSpeed = parseFloat(document.getElementById('xSpeed').value);
+    yRotationSpeed = parseFloat(document.getElementById('ySpeed').value);
+    objectSize = parseFloat(document.getElementById('objectSize').value);
+    currentShape = document.getElementById('shapeSelect').value;
+
+    updateWireframe();
+};
+
+// Listen for changes in input fields and shape selector
+xSpeedInput.querySelector("input").addEventListener('input', handleInputChange);
+ySpeedInput.querySelector("input").addEventListener('input', handleInputChange);
+objectSizeInput.querySelector("input").addEventListener('input', handleInputChange);
+shapeSelector.querySelector("select").addEventListener('change', handleInputChange);
+
 // Create an animation loop
 const animate = function () {
     requestAnimationFrame(animate);
 
-    // Rotate the pyramid
-    pyramid.rotation.y += 0.01;
-    pyramid.rotation.x += 0.01;
+    // Rotate the object based on user-defined speeds
+    wireframe.rotation.y += yRotationSpeed;
+    wireframe.rotation.x += xRotationSpeed;
 
     renderer.render(scene, camera);
 };
